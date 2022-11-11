5a8f91387a7ef0dc69f4a553e232989d29c35496
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-11-11 14:41:25 +0100

    Aggiunge immagine presa dai data

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const {createApp}=Vue;
 createApp({
     data(){
         return{
-            message: "Hello World"
+            message: "Hello World",
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGNlsLSkmfITBapa7_DLvBCIhy872SeX0Eng&usqp=CAU",
+            imgName : "earth"
         }
     }
 }).mount("#app");
